9a3a6604635fff400fabbf612db72fce776b6284
Azza Sallam <https://github.com/AzzaSallam>
2023-09-12 18:42:52 +0300

    edit logo height

--- src/Component/MainHeader.jsx
+++ src/Component/MainHeader.jsx
@@ -20,7 +20,7 @@ export default function MainHeader() {
         <div className='bg-white border-b shadow-sm sticky top-0 z-50 '>
             <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
                 <div>
-                    <img src={logoimg} alt='logo' className='h-5 cursor-pointer' onClick={()=>navigate('/')}/>
+                    <img src={logoimg} alt='logo' className='h-7 cursor-pointer' onClick={()=>navigate('/')}/>
                 </div>
                 <div>
                     <ul className='flex space-x-10  font-semibold text-gray-500 '>
